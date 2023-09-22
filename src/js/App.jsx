04a39c3787caa8f50b09e1671c84074e37132a4e
MelanieLopezAlfaro <107006490+MelanieLopAl@users.noreply.github.com>
2023-09-22 17:07:00 -0600

    project basis

---
+++ src/js/App.jsx
@@ -0,0 +1,16 @@
+import { createRoot } from "react-dom/client";
+import { Header } from './components/react/Header';
+import { MainContent } from "./components/react/MainContent";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <MainContent />
+    </>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
